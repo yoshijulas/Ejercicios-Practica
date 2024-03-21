11c694f20c9ad36a8df43a0d66bbc487e9bfb843
yoshijulas <yoshijulas@live.com>
2024-03-21 02:25:14 -0600

    Enteros distintos

---
+++ 2024/concurso_dia_1/p2_enteros/src/main.rs
@@ -0,0 +1,16 @@
+fn read_input() -> String {
+    let mut input_str = String::new();
+    std::io::stdin()
+        .read_line(&mut input_str)
+        .expect("Failed to read line");
+    input_str
+}
+
+// No se entiende el ejercicio
+// Enteros distintos
+// 4
+// 1 1 2 3
+
+// 1 1 2 3
+
+fn main() {}
